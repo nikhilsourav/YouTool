dff018c48f5ffa562c56c91302ed84b8dee14af5
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-06 12:24:31 +0530

    responsive render

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,23 @@
 const Main = () => {
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        document.querySelector(".response").style.display = 'block';
+    }
+
     return (
         <section className="home" id="home">
             <div className="container home-content">
                 <h1>Search Playlist</h1>
                 <form action="">
                     <input type="text" placeholder="youtube playlist link" />
-                    <button className="btn home-btn">GO</button>
+                    <button className="btn home-btn" onClick={handleClick}>GO</button>
                 </form>
+                <div className="response">
+                    <p>property: value</p>
+                    <p>property: value</p>
+                    <p>property: value</p>
+                </div>
             </div>
         </section>
     );
